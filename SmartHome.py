d14a7ceff3129f10fd0322189821c5262520b913
Joe-cyber <caliendo.gioacchino00@gmail.com>
2022-12-22 10:35:48 +0100

    test check occupancy false green

--- SmartHome.py
+++ SmartHome.py
@@ -57,7 +57,7 @@ class SmartHome:
         Checks whether the infrared distance sensor on the ceiling detects something in front of it.
         :return: True if the infrared sensor detects something, False otherwise.
         """
-        return True
+        return GPIO.input(self.INFRARED_PIN) == 0
 
     def manage_light_level(self) -> None:
         """
