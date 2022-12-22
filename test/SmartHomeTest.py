42d2812221f6e174373aae3119920e8385f7f51c
Joe-cyber <caliendo.gioacchino00@gmail.com>
2022-12-22 12:22:08 +0100

    test temperature out of range 2 green

--- test/SmartHomeTest.py
+++ test/SmartHomeTest.py
@@ -69,8 +69,15 @@ class SmartHomeTest(unittest.TestCase):
         self.assertFalse(window_open)
 
     @patch('mock.adafruit_dht.DHT11.temperature', new_callable=PropertyMock)
-    def test_manage_window_temperature_out_of_range(self, mock_temperature):
+    def test_manage_window_temperature_out_of_range_1(self, mock_temperature):
         mock_temperature.side_effect = [16, 20]
+        self.sm.manage_window()
+        window_open = self.sm.window_open
+        self.assertFalse(window_open)
+
+    @patch('mock.adafruit_dht.DHT11.temperature', new_callable=PropertyMock)
+    def test_manage_window_temperature_out_of_range_2(self, mock_temperature):
+        mock_temperature.side_effect = [18, 31]
         self.sm.manage_window()
         window_open = self.sm.window_open
         self.assertFalse(window_open)
